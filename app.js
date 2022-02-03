61086d47905f1b1348c7ef1dc104e7d453c861b9
A-0x7a69 <98164819+A-0x7a69@users.noreply.github.com>
2022-02-03 15:37:28 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3');
+const { exit } = require('process');
+const { Console } = require('console');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+var timestampOptions = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
+
+var mydb = new sqlite3.Database('./mcu.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err && err.code == "SQLITE_CANTOPEN") {
+        console.log("[WARN] sqlite3 db not found, creating...");
+        createDatabase();
+        return;
+        } else if (err) {
+            console.log("[ERROR] " + err);
+            process.exit();
+    }
+    console.log("[INFO] sqlite3 active");
+});
+
+function createDatabase() {
+    var newdb = new sqlite3.Database('mcu.db', (err) => {
+        if (err) {
+            console.log("Getting error " + err);
+            exit(1);
+        }
+        createTables(newdb);
+    });
+}
+
+function createTables(db) {
+    db.exec(`
+        CREATE TABLE dx_servers(
+            server_id INT PRIMARY KEY NOT NULL,
+            server_name TEXT NOT NULL,
+            reachable BOOLEAN NOT NULL,
+            latency INT NOT NULL,
+            cards_min INT NOT NULL
+        );
+        INSERT INTO dx_servers(server_id, server_name, reachable, latency, cards_min)
+        VALUES(1, 'PS01', TRUE, 94, 23);
+    `, ()=> { console.log("[INFO] Created tables")})   
+};
+
+/*
+* An array where each element is a server object with each row being a property
+*/
+function runQuery(req, res, db, serverDate_) {
+
+    let retArray = [];
+
+    db.all(`
+        SELECT server_id, server_name, reachable, latency, cards_min FROM dx_servers WHERE server_id = ?;
+    `,1, (err, rows)=>{
+        rows.forEach(row =>{
+            let iServer = {
+                iId: row.server_id,
+                iName: row.server_name,
+                iReachable: row.reachable,
+                iLatency: row.latency,
+                iCardsMin: row.cards_min
+            };
+            retArray.push(iServer);
+            console.log(retArray);
+        });
+        console.log(retArray);
+        res.render('monitor', {
+            serverDate: serverDate_,
+            serverId: retArray[0].iId,
+            serverName: retArray[0].iName,
+            serverReach: "a",
+            serverLat: "a",
+            serverCards: "a"
+        });
+    }); 
+}
+
+app.get('/monitor', (req, res)=>{
+    var today = new Date();
+    var serverDate_ = today.toLocaleDateString("en-US", timestampOptions) + " - " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    
+    runQuery(req, res, mydb, serverDate_);
+    
+    
+    
+
+})
+
+app.get('/stats', (req, res)=>{
+    var today = new Date();
+    var serverDate_ = today.toLocaleDateString("en-US", timestampOptions) + " - " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    res.render('stats', {
+        serverDate: serverDate_
+    });
+})
+
+app.listen(3000, ()=>{
+    console.log('Server now listening on port 3000.');
+})
+
---
+++ public/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  //$(".server-detail-list").slideUp();
+
+  // HIGHLIGHT MOUSEOVER LIST ELEMENT
+  $(".server-list-item").mouseenter(function() {
+    $(this).addClass("active");
+  });
+  $(".server-list-item").mouseleave(function() {
+    $(this).removeClass("active");
+  });
+
+  // POPOVER
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+
+  // SERVER LIST SHOW/HIDE DETAILS
+  $(".server-detail").click(function() {
+    $(this).siblings(".server-detail-list").slideToggle();
+  });
+
+});
